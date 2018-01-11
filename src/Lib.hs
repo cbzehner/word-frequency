06d3a152451af86fbd801dee2b011a38d9c79a17
Christopher Zehner <cbzehner@gmail.com>
2018-01-10 23:18:15 -0800

    Basic working counter

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,8 @@ module Lib
     ( wordFrequency
     ) where
 
+import qualified Data.HashMap.Strict as HashMap
 import System.IO (hGetContents, IOMode(..), withFile)
-import Data.HashMap.Strict (fromListWith)
 
 data Path = Directory | File
 
@@ -13,7 +13,7 @@ wordFrequency :: IO ()
 wordFrequency = do
   withFile wordsFilePath ReadMode (\handle -> do
     contents <- hGetContents handle
-    putStrLn contents
-    putStrLn $ lines contents
-    putStrLn $ fromListWith (+) (map (\x -> (x, 1)) (lines contents))
+    -- TODO: use filterWithKey to filter both short words and low word counts
+    putStrLn $ unlines $ map (\(k, v) -> k ++ ": " ++ show v) $ HashMap.toList $
+      HashMap.fromListWith (+) (map (\x -> (x, 1)) (lines contents))
                                   )
