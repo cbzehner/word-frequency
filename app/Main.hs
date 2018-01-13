c29480835755773358ec70dff87182b11abf5910
Christopher Zehner <cbzehner@gmail.com>
2018-01-12 20:08:17 -0800

    Trying to figure out parsing command line args

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,41 @@
 module Main where
 
+{-- External Packages --}
+import Control.Monad (when)
+import Options.Applicative
+import Data.Semigroup
+import System.Console.GetOpt
+import System.Directory (getCurrentDirectory)
+import System.Environment (getArgs)
+import System.Exit
+import System.FilePath (isValid)
+
+{-- Internal Packages --}
 import Lib
 
+data Options = Options
+  { path :: FilePath
+  , wordLength :: Int
+  , count :: Int
+  } deriving (Show)
+
+{-
+defaultOptions :: Options
+defaultOptions = Options
+  { path = defaultPath
+  , wordLength = 17
+  , count = 1
+  }
+  -}
+
+defaultPath = ['.'] :: FilePath
 wordsFilePath = "/usr/share/dict/words" :: FilePath
 wordsFilePath2 = "/usr/share/dict/words" :: FilePath
 files = [wordsFilePath, wordsFilePath2] :: [FilePath] -- A directory
-minWordLength = 17
-minCount = 1
 
 main :: IO ()
-main = wordFrequency files minWordLength minCount
+main = do -- wordFrequency files minWordLength minCount
+  args <- getArgs
+  print args
+
+-- parseArgs :: IO ([String], Int)
