f288b349af02f5d6421d23daaafc4603ff252a79
Christopher Zehner <cbzehner@gmail.com>
2018-01-20 16:53:44 -0800

    Cleanup some path-related oversights

--- src/System/Directory/Recursive.hs
+++ src/System/Directory/Recursive.hs
@@ -5,6 +5,7 @@ module System.Directory.Recursive
 -- TODO:
 -- Look at Real World Haskell suggestions for how to improve this.
 -- Look into switching from a home rolled solution to directory-tree
+-- Filter out dotfiles (folders and files starting with ".")
 
 import Control.Monad (join, mapM)
 import System.Directory
@@ -19,15 +20,17 @@ import System.FilePath (combine, joinPath, splitPath)
 data PathType = Directory | File | Invalid
   deriving (Eq, Ord, Show, Enum)
 
+-- | Get all filenames recursively from a given path or directory
+-- TODO: Add test for cases like "", "~", "~/", "/" and relative and abs paths.
 findAllFileNames :: FilePath -> IO [FilePath]
 findAllFileNames path = do
   absolutePath <- expandTilde path
   pathType <- getPathType absolutePath
   case pathType of
     Invalid -> exitFailure -- TODO: Better error message on invalid paths
-    File -> pure [path]
+    File -> pure [absolutePath]
     Directory -> fmap concat $ join $
-      mapM findAllFileNames <$> getFilesFromDirectory path
+      mapM findAllFileNames <$> getFilesFromDirectory absolutePath
 
 getFilesFromDirectory :: FilePath -> IO [FilePath]
 getFilesFromDirectory path = map (combine path) <$> listDirectory path
@@ -42,6 +45,8 @@ getPathType path = do
        then pure File
        else pure Invalid
 
+-- | Expand the ~ in a file path with the current user's home directory.
+-- TODO: Add test for Empty FilePath and relative and absolute paths. Unicode?
 expandTilde :: FilePath -> IO FilePath
 expandTilde path = if head (splitPath path) == "~/"
    then do
