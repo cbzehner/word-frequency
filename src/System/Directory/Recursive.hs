b0b9331aaa763c5e75a476b3737d1b20a829c8c3
Christopher Zehner <cbzehner@gmail.com>
2018-01-20 16:32:42 -0800

    Handle relative file paths by making them absolute.

--- src/System/Directory/Recursive.hs
+++ src/System/Directory/Recursive.hs
@@ -1,30 +1,38 @@
--- TODO:
---       Handle relative file paths by making them absolute.
---       Look at Real World Haskell suggestions for how to improve this.
---       Look into switching from a home rolled solution to directory-tree
 module System.Directory.Recursive
   ( findAllFileNames
   ) where
 
+-- TODO:
+-- Look at Real World Haskell suggestions for how to improve this.
+-- Look into switching from a home rolled solution to directory-tree
+
 import Control.Monad (join, mapM)
-import System.Directory (doesDirectoryExist, doesFileExist, listDirectory)
+import System.Directory
+  ( doesDirectoryExist
+  , doesFileExist
+  , getHomeDirectory
+  , listDirectory
+  )
 import System.Exit
-import System.FilePath (combine)
+import System.FilePath (combine, joinPath, splitPath)
 
 data PathType = Directory | File | Invalid
   deriving (Eq, Ord, Show, Enum)
 
 findAllFileNames :: FilePath -> IO [FilePath]
 findAllFileNames path = do
-  pathType <- getPathType path
+  absolutePath <- expandTilde path
+  pathType <- getPathType absolutePath
   case pathType of
     Invalid -> exitFailure -- TODO: Better error message on invalid paths
     File -> pure [path]
-    Directory -> fmap concat $ join $ mapM findAllFileNames <$> getFilesFromDirectory path
+    Directory -> fmap concat $ join $
+      mapM findAllFileNames <$> getFilesFromDirectory path
 
 getFilesFromDirectory :: FilePath -> IO [FilePath]
 getFilesFromDirectory path = map (combine path) <$> listDirectory path
 
+getPathType :: FilePath -> IO PathType
 getPathType path = do
   directoryExists <- doesDirectoryExist path
   fileExists <- doesFileExist path
@@ -33,3 +41,10 @@ getPathType path = do
   else if fileExists
        then pure File
        else pure Invalid
+
+expandTilde :: FilePath -> IO FilePath
+expandTilde path = if head (splitPath path) == "~/"
+   then do
+     home <- getHomeDirectory
+     pure $ joinPath $ home : tail (splitPath path)
+   else pure path
